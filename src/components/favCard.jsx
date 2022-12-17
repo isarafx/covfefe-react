634a824ba04525d5662a4382dca116fecd434260
Phichet <isarawn@gmail.com>
2022-12-17 14:24:08 +0700

    Update Favcard

--- src/components/favCard.jsx
+++ src/components/favCard.jsx
@@ -2,40 +2,40 @@ import React from 'react'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-export default function FavCard(key='/',brewer='', name='lorem ipsum', commu=0, fav=0) {
-    let heart = "Tool_icon"
-    let community = "Tool_icon"
-    if(fav){
-        heart = "Tool_Faved"
-    }
-    if(commu){
-        community = "Tool_Shared"
-    }
-    // let favlist = JSON.parse(localStorage.getItem('favorite'))['items']
+export default function FavCard(key = '/', brewer = '', name = 'lorem ipsum', commu = 0, fav = 0) {
+  let heart = "Tool_icon"
+  let community = "Tool_icon"
+  if (fav) {
+    heart = "Tool_Faved"
+  }
+  if (commu) {
+    community = "Tool_Shared"
+  }
+  // let favlist = JSON.parse(localStorage.getItem('favorite'))['items']
   return (
     <div id="method_result_card">
-            <div className="row">
-              <div className="col"><Link to={`/brew-guide/${key}`}><a href="">
-                <div className="card" style={{ height: '65px', background: 'rgba(255,255,255,0)', borderStyle: 'none' }}>
-                  <div className="card-body fcard_body">
-                    <div className="row">
-                      <div className="col d-flex justify-content-center method_result_col">
-                        <div className="method_result_ico_border"><img className="method_result_ico" src="assets/img/{brewer}.png" /></div>
-                      </div>
-                      <div className="col d-flex align-items-center" style={{ maxWidth: '550px', minWidth: '200px' }}>
-                        <p className="recipe_title2">{name}<br /></p>
-                      </div>
-                    </div>
-                  </div>
+      <div className="row">
+        <div className="col"><Link to={`/brew-guide/${key}`}><a href="">
+          <div className="card" style={{ height: '65px', background: 'rgba(255,255,255,0)', borderStyle: 'none' }}>
+            <div className="card-body fcard_body">
+              <div className="row">
+                <div className="col d-flex justify-content-center method_result_col">
+                  <div className="method_result_ico_border"><img className="method_result_ico" src="assets/img/{brewer}.png" /></div>
                 </div>
-              </a></Link></div>
-            </div>
-            <div className="row">
-              <div className="col">
-                <div className="btn-group d-flex" role="group" style={{ width: '100%' }}><button className="btn btn-primary" data-bss-hover-animate="jello" id="Tool_color" type="button"><i className="fa fa-pencil" id="Tool_icon" /></button><a className="btn btn-primary" role="button" data-bss-hover-animate="jello" id="Tool_color" href="Community_shared.html"><i className="fas fa-share" id={community} /></a><button className="btn btn-primary" data-bss-hover-animate="jello" id="Tool_color" type="button"><i className="fas fa-heart-broken" id={heart} /></button><button className="btn btn-primary" data-bss-hover-animate="jello" id="Tool_color" type="button"><i className="fas fa-trash" id="Tool_icon" /></button></div>
+                <div className="col d-flex align-items-center" style={{ maxWidth: '550px', minWidth: '200px' }}>
+                  <p className="recipe_title2">{name}<br /></p>
+                </div>
               </div>
             </div>
           </div>
-          
+        </a></Link></div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <div className="btn-group d-flex justify-content-end" role="group" style={{ width: '100%' }}><button className="btn btn-primary" data-bss-hover-animate="jello" id="Fav_Tool_color" type="button"><i className="fas fa-heart-broken" id="Tool_Faved" /></button></div>
+        </div>
+      </div>
+    </div>
+
   )
 }
--- src/pages/profile-edit.jsx
+++ src/pages/profile-edit.jsx
@@ -15,50 +15,41 @@ export default function ProfileEdit() {
   const token = localStorage.getItem('token')
   return (
     <div>
-  <div className="div_back"><a href="javascript:history.back()"><i className="icon ion-android-arrow-back" id="Back_icon" /></a></div>
-  <div id="main_template">
-    <div className="container profile_container">
-      <form id="Eprofile_card">
-        <div className="row" style={{textAlign: 'center', marginBottom: '20px'}}>
-          <div className="col d-flex justify-content-center">
-            <div className="Eavarta_icon_border"><img id="Eavarta_icon" src="assets/img/AvatarIcon.jpg" /></div>
-          </div>
-        </div>
-        <div className="row" style={{marginBottom: '10px'}}>
-          <div className="col">
-            <div className="Ecard" style={{height: '92px'}}>
-              <div className="d-inline-flex" style={{width: '100%', marginTop: '5px'}}><img className="ae_legend" src="assets/img/legend_picture.png" />
-                <p id="Etitle">{t("PEtext01")}</p>
+      <div className="div_back"><a href="javascript:history.back()"><i className="icon ion-android-arrow-back" id="Back_icon" /></a></div>
+      <div id="main_template">
+        <div className="container profile_container">
+          <form id="Eprofile_card">
+            <div className="row" style={{ textAlign: 'center', marginBottom: '20px' }}>
+              <div className="col d-flex justify-content-center">
+                <div className="Eavarta_icon_border"><img id="Eavarta_icon" src="assets/img/AvatarIcon.jpg" /></div>
               </div>
-              {/* <input className="form-control pAvarta_input" type="file" id="pAvarta_input" accept="image/*" /> */}
-              <form id="image_upload" method="post" action="https://q27z6n.deta.dev/users/images" enctype="multipart/form-data">
-                  <input type="hidden" name="token" value={token} />
-                  <input className="form-control pAvarta_input" type="file" name="file" accept="image/png, image/jpeg" />
-                  <input type="submit" />
-              </form>
             </div>
-          </div>
-        </div>
-        <div className="row" style={{marginBottom: '25px'}}>
-          <div className="col">
-            <div className="Ecard" style={{height: '92px'}}>
-              <div className="d-inline-flex" style={{width: '100%', marginTop: '5px'}}><img className="ae_legend" src="assets/img/legend_rename.png" />
-                <p id="Etitle">{t("PEtext02")}</p>
-              </div><input className="form-control" type="text" id="Einput" placeholder={t("PEtext03")} />
+            <div className="row" style={{ marginBottom: '10px' }}>
+              <div className="col">
+                <div className="Ecard" style={{ height: '92px' }}>
+                  <div className="d-inline-flex" style={{ width: '100%', marginTop: '5px' }}><img className="ae_legend" src="assets/img/legend_picture.png" />
+                    <p id="Etitle">{t("PEtext01")}</p>
+                  </div>
+                  {/* <input className="form-control pAvarta_input" type="file" id="pAvarta_input" accept="image/*" /> */}
+                  <form id="image_upload" method="post" action="https://q27z6n.deta.dev/users/images" enctype="multipart/form-data">
+                    <input type="hidden" name="token" value={token} />
+                    <input className="form-control pAvarta_input" type="file" name="file" accept="image/png, image/jpeg" />
+                    <input type="submit" />
+                  </form>
+                </div>
+              </div>
             </div>
-          </div>
+            <div className="row">
+              <div className="col" style={{ textAlign: 'center' }}><a className="btn btn-primary" role="button" id="Ecancel" href="Profile.html">{t("Cancel99")}</a><button className="btn btn-primary" id="Esubmit" type="submit">{t("Confirm99")}</button></div>
+            </div>
+          </form>
         </div>
-        <div className="row">
-          <div className="col" style={{textAlign: 'center'}}><a className="btn btn-primary" role="button" id="Ecancel" href="Profile.html">{t("Cancel99")}</a><button className="btn btn-primary" id="Esubmit" type="submit">{t("Confirm99")}</button></div>
-        </div>
-      </form>
+      </div>
+      <div className="d-flex" id="Header">
+        <p id="header_paragraph">{t("Ptext01")}</p>
+      </div>
+      <div className="d-flex" id="Footer" />
     </div>
-  </div>
-  <div className="d-flex" id="Header">
-    <p id="header_paragraph">{t("Ptext01")}</p>
-  </div>
-  <div className="d-flex" id="Footer" />
-</div>
 
 
   )
