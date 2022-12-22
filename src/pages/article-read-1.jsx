e0a3537f3d81c40031d45d4afdb0cea73d54b269
Phichet <isarawn@gmail.com>
2022-12-23 00:50:26 +0700

    update3

--- src/pages/article-read-1.jsx
+++ src/pages/article-read-1.jsx
@@ -17,10 +17,11 @@ import { encode, decode } from 'js-base64';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import axios from 'axios';
+import { useTranslation } from 'react-i18next'
 
 export default function ArticleRead() {
 
-
+  const { t, i18n } = useTranslation();
   const [data, setData] = useState('')
   const { id } = useParams();
   const [contents, setContents] = useState('')
